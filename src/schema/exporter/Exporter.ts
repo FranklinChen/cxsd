0a8c72420c443f4310f8ae21c0ab375ab46fa9ec
Franklin Chen <franklinchen@franklinchen.com>
2019-06-14 16:04:56 -0400

    Update dependencies and make compile again.

--- src/schema/exporter/Exporter.ts
+++ src/schema/exporter/Exporter.ts
@@ -1,7 +1,5 @@
 // This file is part of cxsd, copyright (c) 2015-2016 BusFaster Ltd.
 // Released under the MIT license, see LICENSE.
-import { Packet } from '_debugger';
-
 import * as Promise from 'bluebird';
 import * as path from 'path';
 
